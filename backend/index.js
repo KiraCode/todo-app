2af7ad4ec019ec4f12209017d06b2cba665c8ec8
tejas git <d9489079@gmail.com>
2025-11-09 19:18:14 +0530

    added database configuration to the project

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import db from "./utils/db.js";
+
+const app = express();
+const port = process.env.PORT || 8001;
+
+// middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// db
+db();
+// routes
+// connection
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} ...🥳`);
+});
