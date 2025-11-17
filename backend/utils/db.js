300cef1446f0b4ba56f480868e8ac0706934d55e
tejas git <d9489079@gmail.com>
2025-11-17 21:19:06 +0530

    removing all the files

--- backend/utils/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-import "dotenv/config";
-
-export default function db() {
-  mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => console.log("Database connected successfully...😊"))
-    .catch(() => console.error("Error while connecting the mongodb"));
-}
