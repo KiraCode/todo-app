5156d76fbb015f7c1676945a88d6269489a3aeb3
tejas git <d9489079@gmail.com>
2025-11-22 09:01:23 +0530

    delete task been integrated with frontend

---
+++ frontend/src/api/deleteTaskAPI.js
@@ -0,0 +1,29 @@
+async function deleteTaskAPI(taskId, handleResponse, handleError, setLoading) {
+  setLoading(true);
+  try {
+    const baseUrl = import.meta.env.VITE_APP_API_BASE_URL;
+    const endPoint = `/api/v2/task/${taskId}`;
+    const url = new URL(endPoint, baseUrl);
+
+    const response = await fetch(url, {
+      method: "DELETE",
+    });
+
+    const jsonData = await response.json();
+
+    if (!response.ok) {
+      const errorMessage = jsonData.message || "unknown error occured";
+      throw new Error(errorMessage);
+    }
+
+    handleResponse(jsonData);
+  } catch (error) {
+    const errorMessage =
+      error.response?.data?.message || error.message || "unknown error";
+    handleError(new Error(errorMessage));
+  } finally {
+    setLoading(false);
+  }
+}
+
+export default deleteTaskAPI;
