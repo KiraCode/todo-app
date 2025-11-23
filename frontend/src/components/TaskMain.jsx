44359ee38bd29f9f96a9f09815310556a76250dc
tejas git <d9489079@gmail.com>
2025-11-23 10:34:46 +0530

    added ui components customdatepicker, customized inputfield createtask and no taskpage been added

--- frontend/src/components/TaskMain.jsx
+++ frontend/src/components/TaskMain.jsx
@@ -1,8 +1,16 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import NoTask from "./NoTask";
+import TaskList from "./TaskList";
+import CreateTask from "./CreateTask";
+import EditTask from "./EditTask";
+import ViewTask from "./ViewTask";
+import Loading from "./ui/Loading";
+import fetchTasksAPI from "../api/fetchTask.js";
 
 const TaskMain = () => {
   // we manage current screen/routing through state in a single page application
   const [currComponent, setCurrComponent] = useState("loading");
+  const [task, setTask] = useState([]);
 
   const showNoTaskScreen = useCallback(function () {
     setCurrComponent("noTask");
@@ -15,22 +23,52 @@ const TaskMain = () => {
   const showCreateTaskScreen = useCallback(function () {
     setCurrComponent("createTask");
   }, []);
-  
+
   const showEditTaskScreen = useCallback(function () {
     setCurrComponent("editTask");
   }, []);
-  
+
   const showViewTaskScreen = useCallback(function () {
     setCurrComponent("viewTask");
   }, []);
 
+  // api handling
+  const handleResponse = useCallback(function (responseData) {
+    const extractedTasks = responseData.tasks;
+    setTask(extractedTasks);
+    if (extractedTasks.length) {
+      showTaskListScreen();
+    } else {
+      showNoTaskScreen();
+    }
+  }, []);
+
+  const handleError = useCallback((errorMessage) => {
+    alert(errorMessage);
+    console.log(errorMessage);
+  }, []);
+
+  const fetchAllTasks = useCallback(() => {
+    fetchTasksAPI(handleResponse, handleError);
+  }, [handleResponse, handleError]);
+
+  useEffect(() => {
+    fetchAllTasks();
+  }, [fetchAllTasks]);
   return (
     <>
       {currComponent === "loading" && <Loading />}
       <div id="container-div">
-        {currComponent === "noTask" && <NoTask />}
+        {currComponent === "noTask" && (
+          <NoTask showCreateTaskScreen={showCreateTaskScreen} />
+        )}
         {currComponent === "taskList" && <TaskList />}
-        {currComponent === "createTask" && <CreateTask />}
+        {currComponent === "createTask" && (
+          <CreateTask
+            showTaskListScreen={showTaskListScreen}
+            fetchAllTasks={fetchAllTasks}
+          />
+        )}
         {currComponent === "viewTask" && <ViewTask />}
         {currComponent === "editTask" && <EditTask />}
       </div>
